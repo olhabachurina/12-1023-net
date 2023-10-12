560401383f65b0115f6c65058bd39cb4186c5f9b
Olga Bachurina <bachuri12@ukr.net>
2023-10-12 20:51:59 +0300

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace _12_1023_net
+{
+    [Serializable]
+    internal class Product
+    {
+        
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public float Price { get; set; }
+        public Product( string name, int id, float price)
+        {
+            Name = name;
+            Id = id;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            string pathToFile ="product.json";
+           
+            return $"ID: {Id}, Name: {Name}, Price: {Price}";
+            
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _12_1023_net;
+using Newtonsoft.Json;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+/*
+    Product prod1 = new Product("Apple", 1, 21.45f);
+    string json = JsonConvert.SerializeObject(prod1);
+    Console.WriteLine(json);
+    string content = File.ReadAllText(pathToFile);
+    Product prod2 = JsonConvert.DeserializeObject<Product>(content);
+    Console.WriteLine(prod2);
+    using (FileStream fs = new FileStream("product.bin", FileMode.Open))
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        Product obj = (Product)bf.Deserialize(fs);
+        Console.WriteLine(obj);
+    }
+}
+}
+using (FileStream fs = new FileStream("product.bin", FileMode.Open))
+{
+    BinaryFormatter bf = new BinaryFormatter();
+    Product obj = (Product)bf.Deserialize(fs);
+    foreach (var el in obj.ints)
+    {
+        Console.WriteLine(el);
+    }
+}*/
+Console.WriteLine("Введите путь к файлу:");
+string filePath = Console.ReadLine();
+
+try
+{
+    if (File.Exists(filePath))
+    {
+        string fileContent = File.ReadAllText(filePath);
+        Console.WriteLine($"Contents of the file {filePath}:\n{fileContent}");
+    }
+    else
+    {
+        Console.WriteLine($"The file {filePath} does not exist.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"error: {ex.Message}");
+}
+    
